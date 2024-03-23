b5f6e2410cbe6f85994f64e43ba6579673113e7c
osamanazar47 <osamanazar47@gmail.com>
2024-03-23 14:47:52 +0200

    create

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #a script that creates a file in /tmp
 
-file {'/tmp/school':
+file { '/tmp/school':
+  content =>'I love Puppet',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love puppet',
-  mode    => '0744',
 }
