2d2d0d9416124980e05af39717142b348fb4f404
osamanazar47 <osamanazar47@gmail.com>
2024-03-23 15:05:32 +0200

    flaskinstall

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
 # install flask version (2.1.0) from pip3
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+  ensure   => '2.1.0', #ensure flask version 2.1.0
+  provider => 'pip3', # the provider of flask is pip3
 }
