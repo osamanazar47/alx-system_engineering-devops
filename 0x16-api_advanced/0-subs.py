dfed72273deead993d04699d1486c1c3e6fa34d2
osamanazar47 <osamanazar47@gmail.com>
2024-05-07 13:45:20 +0300

    fix_number_of_subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""a python script for querying the Reddit API"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """a function that returns the number of subscribers for
-    a given subreddit"""
-
+    """a function that Returns the total number of subscribers on a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url,
-                            headers={"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"},
-                            allow_redirects=False)
-
-    if response.status_code == 200:
-        results = response.json().get("data")
-        return results.get("subscribers")
-    else:
-        return 0  # Return 0 if the subreddit doesn't exist or there's an error
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
