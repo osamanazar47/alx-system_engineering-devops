55c1cad1c19bda554776954be754b7c320e217c4
osamanazar47 <osamanazar47@gmail.com>
2024-05-08 00:33:10 +0300

    hot_posts

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,13 +2,20 @@
 """a recursive function for querying from reddit api"""
 import requests
 
-def recurse(subreddit, hot_list=[], after=None):
-    """Recursively queries the Reddit API and returns a list containing
-    the titles of all hot articles for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100&after={after}"
-    headers = {"User-Agent": "Python Script"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
 
+def recurse(subreddit, hot_list=[], after=None):
+    """Recursively queries the Reddit API and returns
+    a list containing the titles of all hot articles for a given subreddit.
+    """
+    params = {"limit": 100}
+    if after:
+        params["after"] = after
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Python Script"}
+
+    response = requests.get(url, params=params,
+                            headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return None
 
